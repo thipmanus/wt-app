fcf06d1f8dba9ad6732e52ee95900b27ae6d2c8a
thipmanus <5910110148@psu.ac.th>
2018-09-24 11:50:44 +0700

    ตอนที่ 4-5

--- components/Forecast.js
+++ components/Forecast.js
@@ -3,12 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class Forecast extends React.Component {
     render() {
     return (
-    <View>
-    <Text>{this.props.main}</Text>
-    <Text>{this.props.description}</Text>
-    <Text>{this.props.temp}</Text>
-    <Text>°C</Text>
+    <View >
+        <Text style={styles.main}>{this.props.main}</Text>
+        <Text style={styles.description}>{this.props.description}</Text>
+        <Text style={styles.temp}>{this.props.temp}°C</Text>
     </View>
     );
     }
 }
+
+const styles = StyleSheet.create({
+    main: {textAlign:'center', color:'#E44179', fontSize:50},
+    description: {textAlign:'center', color:'#C32121', fontSize:50},
+    temp: {textAlign:'center', color:'#59B5F0', fontSize:50},
+});  
--- components/Weather.js
+++ components/Weather.js
@@ -3,19 +3,37 @@ import { StyleSheet, Text, View ,ImageBackground} from 'react-native';
 import Forecast from './Forecast';
 export default class Weather extends React.Component {
   
-constructor(props) {
-    super(props);
-    this.state = {
+    constructor(props) {
+        super(props);
+        this.state = {
+        forecast: {
+            main: '-', description: '-', temp: 0
+        }
+        }
+    }
+    fetchData = () => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.props.zipCode},th&units=metric&APPID=fd68c0f2039c5a25f666a9ff374bc93e`)
+        .then((response) => response.json())
+        .then((json) => {
+        this.setState(
+    {
     forecast: {
-        main: '-', description: '-', temp: 0
+        main: json.weather[0].main,
+        description: json.weather[0].description,
+        temp: json.main.temp
     }
+    });
+    })
+    .catch((error) => {
+    console.warn(error);
+    });
     }
-}
+    componentDidMount = () => this.fetchData()
  render() {
  return (
-    <View style={styles.container}>
+    <View style={styles.flex}>
     <ImageBackground source={require('../img/bg.jpeg')} style={styles.backdrop}>
-    <Text>Zip code is {this.props.zipCode}.</Text>
+    <Text style={styles.zipCode}>Zip code is {this.props.zipCode}.</Text>
     <Forecast {...this.state.forecast} />
     </ImageBackground>
     </View>
@@ -26,4 +44,6 @@ constructor(props) {
 const styles = StyleSheet.create({
     container: { paddingTop: 25 },
     backdrop: { width: '100%', height: '100%'},
+    zipCode: {textAlign:'center', color:'#00D084', fontSize:20},
+    flex: {flex: 1,flexDirection: 'column',justifyContent: 'center',alignItems: 'center' },
 });
